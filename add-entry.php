a2760c8e5a360a2bf6da9e76c6e6b828f87b59a8
XIB1 <fredrik.snarbacka@gmail.com>
2023-02-12 17:36:12 +0200

    add entry fix

--- add-entry.php
+++ add-entry.php
@@ -9,8 +9,21 @@
   
   // Connect to database and insert the new entry
   $conn = new mysqli("34.88.150.1", "app-user", "983298", "gym-db");
+
+  if (!$conn) {
+    header("HTTP/1.1 500 Internal Server Error");
+    echo "Error connecting to database: " . mysqli_connect_error();
+    exit;
+  }
+
   $sql = "INSERT INTO table (Date, Time, Exercise, Weight, Sets, Reps, Effort)
           VALUES ('$field1', '$field2', '$field3', '$field4', '$field5', '$field6', '$field7')";
-  $conn->query($sql);
+  if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+  }
+  else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+  
   $conn->close();
 ?>
