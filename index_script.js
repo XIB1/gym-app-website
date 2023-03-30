1c609320c19add56dd283b93722a93e1ec65e265
XIB1 <fredrik.snarbacka@gmail.com>
2023-03-30 22:10:48 +0300

    login + token working

--- index_script.js
+++ index_script.js
@@ -1,3 +1,19 @@
+//import  { jwtdecode } from "./node_modules/jwt-decode/build/jwt-decode.js";
+
+
+function parseJwt (token) {
+  var base64Url = token.split('.')[1];
+  var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+  }).join(''));
+
+  return JSON.parse(jsonPayload);
+}
+
+
+
+/*
 function onSignIn(id_token) {
     var xhr = new XMLHttpRequest();
     xhr.open('POST', 'verify-login.php');
@@ -12,14 +28,32 @@ function onSignIn(id_token) {
 
 function sign2() {
     console.log("test1");
+
     const client = google.accounts.oauth2.initTokenClient({
     client_id: '636033609809-dt5m30p5qurko02s9docsqlnoc6232nb.apps.googleusercontent.com',
     scope: 'https://www.googleapis.com/auth/userinfo.profile',
-    //ux_mode: 'popup',
     callback: (tokenResponse) => {
         console.log(tokenResponse);
         onSignIn(tokenResponse.access_token);
     },
     });
     client.requestAccessToken();
+};
+*/
+window.onload = function() {
+  var currentDate = new Date();
+  var expirationDate = new Date(currentDate.getTime() + (14 * 24 * 60 * 60 * 1000));
+
+  google.accounts.id.initialize({
+    client_id: "636033609809-dt5m30p5qurko02s9docsqlnoc6232nb.apps.googleusercontent.com",
+    callback: (response) => {
+      console.log(parseJwt(response.credential));
+      document.cookie = "googleAuth=" + response.credential + "; expires=" + expirationDate.toUTCString() + "; path=/";
+    },
+    auto_select: false,
+  });
+  google.accounts.id.renderButton(
+    document.getElementById("button"),
+    { theme: "outline", size: "large" }
+  );
 };
--- verify-login.php
+++ verify-login.php
@@ -1,23 +1,83 @@
 <?php
-require_once 'C:/repos/gym-app-website/vendor/autoload.php'; // include the Google API PHP client library
 
-// set up the client object
-$client = new Google_Client(['client_id' => '636033609809-dt5m30p5qurko02s9docsqlnoc6232nb.apps.googleusercontent.com']);
-$client->setApplicationName('gymlog.xyz');
-$client->setAccessType('offline');
-$client->setApprovalPrompt('auto');
-$client->setScopes(['email']);
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
-// verify the id_token
-$id_token = $_POST['id_token'];
-$payload = $client->verifyIdToken($id_token);
-if ($payload) {
-  // the id_token is valid
-  $user_id = $payload['sub'];
-  // use $user_id to authenticate the user on your server
-} else {
-  // the id_token is invalid
-  http_response_code(401);
-  die('Invalid id_token');
+
+require_once 'vendor/autoload.php';
+
+// Start session
+session_start();
+
+// Setup the Google API client
+$client = new Google\Client();
+$client->setApplicationName('Google Login');
+$client->setClientId('636033609809-dt5m30p5qurko02s9docsqlnoc6232nb.apps.googleusercontent.com');
+$client->setClientSecret('############################');
+$client->setRedirectUri('http://localhost:8000');
+$client->setScopes(['email', 'profile']);
+
+// If user is not logged in, redirect to Google login page
+if (!isset($_SESSION['access_token'])) {
+  $auth_url = $client->createAuthUrl();
+  header('Location: ' . filter_var($auth_url, FILTER_SANITIZE_URL));
+  exit;
 }
+
+// If user is logged in, get the access token and user profile
+if (isset($_SESSION['access_token'])) {
+  $client->setAccessToken($_SESSION['access_token']);
+  $oauth2 = new Google\Service\Oauth2($client);
+  $userInfo = $oauth2->userinfo->get();
+  $email = $userInfo->email;
+}
+
+// Connect to the database
+$host = '34.88.150.1';
+$dbname = 'gym-db';
+$username = 'app-user';
+$password = '983298';
+
+try {
+  $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+  exit;
+}
+
+// Check if user exists in the database
+$stmt = $db->prepare("SELECT id FROM users WHERE email=:email");
+$stmt->bindValue(':email', $email, PDO::PARAM_STR);
+$stmt->execute();
+$user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+if ($user) {
+  // User exists in the database
+  $_SESSION['user_id'] = $user['id'];
+} else {
+  // User does not exist in the database, add user
+  $firstName = $userInfo->givenName;
+  $lastName = $userInfo->familyName;
+  $created = date('Y-m-d H:i:s');
+  $modified = date('Y-m-d H:i:s');
+
+  $stmt = $db->prepare("INSERT INTO users (oauth_uid, first_name, last_name, email, created, modified) VALUES (:oauth_uid, :first_name, :last_name, :email, :created, :modified)");
+  $stmt->bindValue(':oauth_uid', $userInfo->id, PDO::PARAM_STR);
+  $stmt->bindValue(':first_name', $firstName, PDO::PARAM_STR);
+  $stmt->bindValue(':last_name', $lastName, PDO::PARAM_STR);
+  $stmt->bindValue(':email', $email, PDO::PARAM_STR);
+  $stmt->bindValue(':created', $created, PDO::PARAM_STR);
+  $stmt->bindValue(':modified', $modified, PDO::PARAM_STR);
+  $stmt->execute();
+
+  $_SESSION['user_id'] = $db->lastInsertId();
+}
+
+// Redirect to the home page
+header('Location: index.php');
+exit;
 ?>
